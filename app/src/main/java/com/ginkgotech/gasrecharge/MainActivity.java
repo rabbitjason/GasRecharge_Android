cbcc4cde7057ad565f0763807d83b03ceb8b6844
rabbitjason <lipple-server@Lipple-ServerdeMac-mini.local>
2016-10-29 16:07:17 +0800

    add function

--- app/src/main/java/com/ginkgotech/gasrecharge/MainActivity.java
+++ app/src/main/java/com/ginkgotech/gasrecharge/MainActivity.java
@@ -3,11 +3,69 @@ package com.ginkgotech.gasrecharge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.koushikdutta.async.AsyncServer;
+import com.koushikdutta.async.AsyncSocket;
+import com.koushikdutta.async.ByteBufferList;
+import com.koushikdutta.async.DataEmitter;
+import com.koushikdutta.async.Util;
+import com.koushikdutta.async.callback.CompletedCallback;
+import com.koushikdutta.async.callback.ConnectCallback;
+import com.koushikdutta.async.callback.DataCallback;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private String protocol = "0567|2001|1|a2131091ffff8115ffffffffffffffffffff01ffffd27600000400ffffffffff7050635a005c5e00320100000010061805ef51000000000202ef0000000000000000000000000000000000000000030094ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00ffffffffffffffffffffffff01201606e4000000000000000000000000000000000000000000000000000000000000000000000000000001000800000000099990929400ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff|20160905|171025|200001|0123456789|75621477";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectServer();
+    }
+
+    private void connectServer() {
+        AsyncServer.getDefault().connectSocket("120.25.121.156", 10020, new ConnectCallback() {
+            @Override
+            public void onConnectCompleted(Exception ex, AsyncSocket socket) {
+                handleConnectCompleted(ex, socket);
+            }
+        });
+    }
+
+    private void handleConnectCompleted(Exception ex, final AsyncSocket socket) {
+        if(ex != null) throw new RuntimeException(ex);
+
+        Util.writeAll(socket, protocol.getBytes(), new CompletedCallback() {
+            @Override
+            public void onCompleted(Exception ex) {
+                if (ex != null) throw new RuntimeException(ex);
+                System.out.println("[Client] Successfully wrote message");
+            }
+        });
+
+        socket.setDataCallback(new DataCallback() {
+            @Override
+            public void onDataAvailable(DataEmitter emitter, ByteBufferList bb) {
+                System.out.println("[Client] Received Message " + new String(bb.getAllByteArray()));
+            }
+        });
+
+        socket.setClosedCallback(new CompletedCallback() {
+            @Override
+            public void onCompleted(Exception ex) {
+                if(ex != null) throw new RuntimeException(ex);
+                System.out.println("[Client] Successfully closed connection");
+            }
+        });
+
+        socket.setEndCallback(new CompletedCallback() {
+            @Override
+            public void onCompleted(Exception ex) {
+                if(ex != null) throw new RuntimeException(ex);
+                System.out.println("[Client] Successfully end connection");
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/ginkgotech/gasrecharge/model/BaseModel.java
@@ -0,0 +1,17 @@
+package com.ginkgotech.gasrecharge.model;
+
+/**
+ * Created by lipple-server on 16/10/29.
+ */
+
+public abstract class BaseModel {
+
+    public int len;
+
+    public String code;
+
+    public abstract String packageData();
+
+    public abstract void parseData(String data);
+
+}
