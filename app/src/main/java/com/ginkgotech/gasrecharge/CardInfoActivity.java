d0ee0e84d759dfc169fd0e8142b0939b17324a92
rabbitjason <lipple-server@Lipple-ServerdeMac-mini.local>
2016-11-01 16:19:20 +0800

    add function

---
+++ app/src/main/java/com/ginkgotech/gasrecharge/CardInfoActivity.java
@@ -0,0 +1,13 @@
+package com.ginkgotech.gasrecharge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_info);
+    }
+}
